1960594d232e6459d41e44b0e7f62d638d0219bf
tinykidtoo <tinykidtoodotcom@yahoo.com>
2020-04-18 10:11:55 -0700

    Fix_Merge_Conflicts

---
+++ Assets/Scripts/OnDamageEffects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDamageEffects : MonoBehaviour
+{
+    [SerializeField] AudioClip universalHitMarker;
+    [SerializeField] AudioSource source;
+
+    public void OnDamage() {
+        //FreezeFrame
+        source.spatialBlend = 0;
+        source.PlayOneShot(universalHitMarker);
+    }
+}
