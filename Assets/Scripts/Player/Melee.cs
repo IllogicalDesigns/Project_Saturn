98852d35126eac24cd9a18fde58d12828320f16a
tinykidtoo <tinykidtoodotcom@yahoo.com>
2020-04-18 12:50:14 -0700

    Melee partial progress

--- Assets/Scripts/Player/Melee.cs
+++ Assets/Scripts/Player/Melee.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Melee : MonoBehaviour
 {
+    private bool canPunch;
+    private string fire;
+
+    public float timeInBetweenPunches = 1f;
+    private float punchTimer;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,13 @@ public class Melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (punchTimer >= 0)
+            punchTimer -= Time.deltaTime;
+
+        if (Input.GetButtonDown(fire) && canPunch && punchTimer <= 0) {
+            //punchDamnIt();
+
+            punchTimer = timeInBetweenPunches;
+        }
     }
 }
