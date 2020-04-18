888131f7fe7c1bba7bc899f519a254968396b161
swampflare <swampflare@users.noreply.github.com>
2020-04-18 12:30:18 -0700

    Add namespaces to most of AI.

--- Assets/Scripts/AI/Archer.cs
+++ Assets/Scripts/AI/Archer.cs
@@ -3,96 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Archer : MonoBehaviour {
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private Transform player;
-    Vector3 target;
+namespace AI {
+    public class Archer : MonoBehaviour {
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Transform player;
+        Vector3 target;
 
-    Vector3 origArea;
+        Vector3 origArea;
 
-    private enum state { wander, chasing, attacking };
-    state currState = state.wander;
+        private enum state {
+            wander,
+            chasing,
+            attacking
+        };
 
-    [SerializeField] float chaseDist = 15f, attackDist = 10f, maxWanderDist = 10f, timeInBetweenAttacks = 1f;
+        state currState = state.wander;
 
-    [SerializeField] GameObject arrowPrefab;
-    [SerializeField] Transform firePoint;
+        [SerializeField] float chaseDist = 15f, attackDist = 10f, maxWanderDist = 10f, timeInBetweenAttacks = 1f;
 
-    float spread = 0.15f;
+        [SerializeField] GameObject arrowPrefab;
+        [SerializeField] Transform firePoint;
 
-    float attackTimer = 0f;
+        float spread = 0.15f;
 
-    private void Start() {
-        origArea = transform.position;
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        WanderInDirection();
-    }
+        float attackTimer = 0f;
 
-    private void Wander() {
-        if (Vector3.Distance(transform.position, player.position) < chaseDist)
-            currState = state.chasing;
-
-        if (Vector3.Distance(transform.position, target) < 1) {
+        private void Start() {
+            origArea = transform.position;
+            agent = gameObject.GetComponent<NavMeshAgent>();
             WanderInDirection();
         }
 
-        agent.isStopped = false;
-    }
+        private void Wander() {
+            if (Vector3.Distance(transform.position, player.position) < chaseDist)
+                currState = state.chasing;
 
-    private void WanderInDirection() {
-        target = wanderPoint(maxWanderDist); //Get new wander point origArea
-        agent.SetDestination(new Vector3(target.x, transform.position.y, target.z));
-    }
+            if (Vector3.Distance(transform.position, target) < 1) {
+                WanderInDirection();
+            }
 
-    Vector3 wanderPoint(float wanderDist) {
-        Vector3 randPoint = Random.insideUnitSphere* wanderDist + origArea;
-        NavMeshHit hit; // NavMesh Sampling Info Container
-
-        // from randomPos find a nearest point on NavMesh surface in range of maxDistance
-        NavMesh.SamplePosition(randPoint, out hit, wanderDist, NavMesh.AllAreas);
-        return hit.position;
-    }
-
-    private void Chasing() {
-        if (Vector3.Distance(transform.position, player.position) < attackDist)
-            currState = state.attacking;
-
-        agent.isStopped = false;
-        agent.SetDestination(player.position);
-    }
-
-    private void Attacking() {
-        agent.isStopped = true;
-
-        if (Vector3.Distance(transform.position, player.position) > attackDist)
-            currState = state.chasing;
-
-        if(attackTimer < 0f) {
-            Vector3 pos = (firePoint.position + (firePoint.right * Random.Range(-spread, spread)));
-            GameObject newArrow = Instantiate(arrowPrefab, pos, firePoint.rotation) as GameObject;
-            newArrow.transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
-            newArrow.SendMessage("SetOwner", -1);  //TODO fucking remove hardcode
-            newArrow.SendMessage("SetDmg", 25);  //TODO this is not sexy
-            newArrow.SendMessage("SetSpeed", 15);
-            attackTimer = timeInBetweenAttacks;
+            agent.isStopped = false;
         }
-        else {
-            attackTimer -= Time.deltaTime;
+
+        private void WanderInDirection() {
+            target = wanderPoint(maxWanderDist); //Get new wander point origArea
+            agent.SetDestination(new Vector3(target.x, transform.position.y, target.z));
         }
-    }
 
-    private void Update() {
-        switch (currState) {
-            case state.chasing:
-                Chasing();
-                break;
-            case state.attacking:
-                Attacking();
-                break;
-            default:
-                Wander();
-                break;
+        Vector3 wanderPoint(float wanderDist) {
+            Vector3 randPoint = Random.insideUnitSphere * wanderDist + origArea;
+            NavMeshHit hit; // NavMesh Sampling Info Container
 
+            // from randomPos find a nearest point on NavMesh surface in range of maxDistance
+            NavMesh.SamplePosition(randPoint, out hit, wanderDist, NavMesh.AllAreas);
+            return hit.position;
+        }
+
+        private void Chasing() {
+            if (Vector3.Distance(transform.position, player.position) < attackDist)
+                currState = state.attacking;
+
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+        }
+
+        private void Attacking() {
+            agent.isStopped = true;
+
+            if (Vector3.Distance(transform.position, player.position) > attackDist)
+                currState = state.chasing;
+
+            if (attackTimer < 0f) {
+                Vector3 pos = (firePoint.position + (firePoint.right * Random.Range(-spread, spread)));
+                GameObject newArrow = Instantiate(arrowPrefab, pos, firePoint.rotation) as GameObject;
+                newArrow.transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+                newArrow.SendMessage("SetOwner", -1); //TODO fucking remove hardcode
+                newArrow.SendMessage("SetDmg", 25); //TODO this is not sexy
+                newArrow.SendMessage("SetSpeed", 15);
+                attackTimer = timeInBetweenAttacks;
+            }
+            else {
+                attackTimer -= Time.deltaTime;
+            }
+        }
+
+        private void Update() {
+            switch (currState) {
+                case state.chasing:
+                    Chasing();
+                    break;
+                case state.attacking:
+                    Attacking();
+                    break;
+                default:
+                    Wander();
+                    break;
+
+            }
         }
     }
 }
--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private Transform target;
+namespace AI {
+    public class Enemy : MonoBehaviour {
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Transform target;
 
-    private void Start() {
-        agent = gameObject.GetComponent<NavMeshAgent>();
-    }
+        private void Start() {
+            agent = gameObject.GetComponent<NavMeshAgent>();
+        }
 
-    private void Update() {
-        agent.SetDestination(target.position);
+        private void Update() {
+            agent.SetDestination(target.position);
+        }
     }
 }
