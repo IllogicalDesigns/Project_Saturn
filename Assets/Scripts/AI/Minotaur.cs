825bbeac42ecf51b15a0be2c9a05be2a02ba68c9
swampflare <swampflare@users.noreply.github.com>
2020-04-18 13:11:45 -0700

    more mino

--- Assets/Scripts/AI/Minotaur.cs
+++ Assets/Scripts/AI/Minotaur.cs
@@ -8,19 +8,18 @@ namespace AI {
       [SerializeField] private Transform player;
       private Vector3 target;
       
-
-      private enum State { Wander, Chasing, Charging };
+      private enum State { Wander, Charging, Recovering };
       State currState = State.Wander;
 
-      [SerializeField] private float chaseDist = 15, attackDist = 1, maxWanderDist = 10, timeInBetweenAttacks = 1;
+      [SerializeField] private float chargeDist = 15, attackDist = 1, maxWanderDist = 10;
 
       private void Start() {
           agent = gameObject.GetComponent<NavMeshAgent>();
       }
 
       private void Wander() {
-          if (Vector3.Distance(transform.position, player.position) < chaseDist)
-              currState = State.Chasing;
+          if (Vector3.Distance(transform.position, player.position) < chargeDist)
+              currState = State.Charging;
 
           if (Vector3.Distance(transform.position, target) < 1) {
               WanderInDirection();
@@ -35,11 +34,25 @@ namespace AI {
       
       Vector3 WanderPoint(float wanderDist) {
           Vector3 randPoint = Random.insideUnitSphere* wanderDist +transform.position;
-          NavMeshHit hit; // NavMesh Sampling Info Container
 
           // from randomPos find a nearest point on NavMesh surface in range of maxDistance
-          NavMesh.SamplePosition(randPoint, out hit, wanderDist, NavMesh.AllAreas);
+          NavMesh.SamplePosition(randPoint, out var hit, wanderDist, NavMesh.AllAreas);
           return hit.position;
+      }
+
+      private void Update() {
+          switch (currState) {
+              case State.Charging:
+                  break;
+              case State.Recovering:
+                  break;
+              case State.Wander:
+                  Wander();
+                  break;
+              default:
+                  Wander();
+                  break;
+          }
       }
 
     }
