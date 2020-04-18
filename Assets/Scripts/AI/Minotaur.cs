3df4ff43de139e76ddeb4669b26f14a645c7b9b6
swampflare <swampflare@users.noreply.github.com>
2020-04-18 13:41:32 -0700

    more mino

--- Assets/Scripts/AI/Minotaur.cs
+++ Assets/Scripts/AI/Minotaur.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,9 +8,9 @@ namespace AI {
       [SerializeField] private NavMeshAgent agent;
       [SerializeField] private Transform player;
       private Vector3 target;
-      
-      private enum State { Wander, Charging, Recovering };
       State currState = State.Wander;
+      
+      private enum State { Wander, Preparing, Charging, Recovering };
 
       [SerializeField] private float chargeDist = 15, attackDist = 1, maxWanderDist = 10;
 
@@ -18,8 +19,10 @@ namespace AI {
       }
 
       private void Wander() {
-          if (Vector3.Distance(transform.position, player.position) < chargeDist)
-              currState = State.Charging;
+          if (Vector3.Distance(transform.position, player.position) < chargeDist) {
+              currState = State.Preparing;
+              return;
+          }
 
           if (Vector3.Distance(transform.position, target) < 1) {
               WanderInDirection();
@@ -33,18 +36,43 @@ namespace AI {
       }
       
       Vector3 WanderPoint(float wanderDist) {
-          Vector3 randPoint = Random.insideUnitSphere* wanderDist +transform.position;
+          Vector3 randPoint = Random.insideUnitSphere * wanderDist + transform.position;
 
           // from randomPos find a nearest point on NavMesh surface in range of maxDistance
           NavMesh.SamplePosition(randPoint, out var hit, wanderDist, NavMesh.AllAreas);
           return hit.position;
       }
 
+      private void Preparing() {
+          target = player.position;
+          agent.SetDestination(target);
+          
+          StartCoroutine(PrepareToCharge());
+      }
+      
+      private void Charging() {
+          if (Vector3.Distance(transform.position, player.position) < attackDist) {
+              // deal damage, knockback, stop + recover
+              currState = State.Recovering;
+          }
+
+          agent.isStopped = false;
+      }
+
+      private IEnumerator PrepareToCharge() {
+          currState = State.Charging;
+          yield return new WaitForSeconds(1);
+      }
+
       private void Update() {
           switch (currState) {
               case State.Charging:
+                  Charging();
                   break;
               case State.Recovering:
+                  break;
+              case State.Preparing:
+                  Preparing();
                   break;
               case State.Wander:
                   Wander();
