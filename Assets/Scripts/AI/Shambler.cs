bd7c20b9b4248bd2eb8e6636ad8d3df69b4e6ceb
swampflare <swampflare@users.noreply.github.com>
2020-04-17 22:01:06 -0700

    Add Shambler behavior.

--- Assets/Scripts/AI/Shambler.cs
+++ Assets/Scripts/AI/Shambler.cs
@@ -7,15 +7,51 @@ public class Shambler : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform target;
-    // Start is called before the first frame update
-    void Start() {
-        agent = gameObject.GetComponent<NavMeshAgent>();
+    private ShamblerState currentState = ShamblerState.Standing;
+
+    private enum ShamblerState {
+        Standing,
+        Attacking,
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void Standing() {
+        agent.isStopped = true;
+        currentState = ShamblerState.Standing;
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance < 5) {
+            Attacking();
+        }
+    }
+
+    private void Attacking() {
+        currentState = ShamblerState.Attacking;
+        agent.isStopped = false;
+
+        var targetPos = target.position;
+        agent.SetDestination(targetPos);
+        
+        float distance = Vector3.Distance(transform.position, targetPos);
+        if (distance >= 5) {
+            Standing();
+        }
+    }
+    
+    void Start() {
+        agent = gameObject.GetComponent<NavMeshAgent>();
         agent.SetDestination(target.position);
-        var pos = transform.position;
-        float distance = Vector3.Distance(pos, pos);
+    }
+
+    void Update() {
+        // agent.SetDestination(target.position);
+        // var pos = transform.position;
+        // float distance = Vector3.Distance(pos, pos);
+        switch (currentState) {
+            case ShamblerState.Standing:
+                Standing();
+                break;
+            case ShamblerState.Attacking:
+                Attacking();
+                break;
+        }
     }
 }
