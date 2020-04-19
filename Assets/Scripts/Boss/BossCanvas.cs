4749ebcdd4a1be19b34fc500544ac6bf2528ce8e
bgray <bgray8898@gmail.com>
2020-04-19 12:49:31 -0700

    Working health bars for bosses.

---
+++ Assets/Scripts/Boss/BossCanvas.cs
@@ -0,0 +1,17 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossCanvas : MonoBehaviour {
+
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Health bossHealth;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        healthSlider.value = bossHealth.PercentHp;
+    }
+}
