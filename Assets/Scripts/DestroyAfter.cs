91cd84e5d8dd12fb08f4adb00eb8cd0ffeaf5058
tinykidtoo <tinykidtoodotcom@yahoo.com>
2020-04-20 16:54:19 -0700

    Music Music Music

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    [SerializeField] float lifetime = 75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
