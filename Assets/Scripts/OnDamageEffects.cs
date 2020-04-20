a3b6a1f0f7b3e4b7fe4e5448df4d7531aeba0b48
tinykidtoo <tinykidtoodotcom@yahoo.com>
2020-04-20 11:39:05 -0700

    Elon has landed on mars

--- Assets/Scripts/OnDamageEffects.cs
+++ Assets/Scripts/OnDamageEffects.cs
@@ -6,15 +6,18 @@ public class OnDamageEffects : MonoBehaviour
 {
     [SerializeField] AudioClip universalHitMarker, MeleePunch;
     [SerializeField] AudioSource source;
+    [SerializeField] SpriteFlipper flipper;
 
     public void OnDamage() {
         //FreezeFrame
+        if (flipper != null) flipper.OnDamage();
         source.spatialBlend = 0;
         source.PlayOneShot(universalHitMarker);
     }
 
     public void OnMeleeDamage() {
         //FreezeFrame
+        if (flipper != null) flipper.OnDamage();
         source.spatialBlend = 0;
         source.PlayOneShot(universalHitMarker);
         source.PlayOneShot(MeleePunch);
