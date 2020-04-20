72c32787d92d361eb998b943b02b95340e8dbd92
tinykidtoo <tinykidtoodotcom@yahoo.com>
2020-04-19 21:00:44 -0700

    Transition to scenes work, but only I know the secret

---
+++ Assets/Scripts/EventOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EventOnTrigger : MonoBehaviour
+{
+    public UnityEvent onTrigger;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+            onTrigger.Invoke();
+    }
+}
