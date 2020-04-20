c0fa298d5265c8279ed167c6ce990c382366a217
bgray <bgray8898@gmail.com>
2020-04-20 08:39:38 -0700

    Menu fixes.

--- Assets/Scripts/LoadLevels.cs
+++ Assets/Scripts/LoadLevels.cs
@@ -5,14 +5,17 @@ public class LoadLevels : MonoBehaviour
 {
     [SerializeField] string levelToLoad;
     public void LoadLevel(string newLevel) {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(newLevel);
     }
 
     public void LoadLevelGenString() {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(levelToLoad);
     }
 
     public void RestartCurrentLevel() {
+        Time.timeScale = 1f;
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
 
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+
+    public void DoQuit() {
+        Application.Quit();
+    }
+    
+}
